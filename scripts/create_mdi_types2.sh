976642b573a01f189a9796fca32f8c168d2bd29c
dfinas <dfinas@gmail.com>
2019-07-01 11:55:45 +0200

    first release

---
+++ scripts/create_mdi_types2.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+#
+# SCRIPT: create_mdi_types.sh
+# AUTHOR: filip@sweagle>com
+# DATE:   25 April 2019
+# REV:    1.1.D (Valid are A, B, D, T, Q, and P)
+#               (For Alpha, Beta, Dev, Test, QA, and Production)
+#
+# PLATFORM: Not platform dependent
+#
+# REQUIREMENTS:	- jq is required for this shell script to work.
+#               (see: https://stedolan.github.io/jq/)
+#				- tested in bash 4.4 on Mac OS X
+#
+# PURPOSE:	Define MDI types with regular expressions.
+#
+# REV LIST:
+#        DATE: DATE_of_REVISION
+#        BY:   AUTHOR_of_MODIFICATION
+#        MODIFICATION: Describe what was modified, new features, etc--
+#
+#
+# set -n   # Uncomment to check script syntax, without execution.
+#          # NOTE: Do not forget to put the # comment back in or
+#          #       the shell script will never execute!
+set -x   # Uncomment to debug this shell script
+#
+##########################################################
+#               FILES AND VARIABLES
+##########################################################
+
+# command line arguments
+this_script=$(basename $0)
+host=${1:-}
+
+##########################################################
+#                    FUNCTIONS
+##########################################################
+
+# arg1: http result (incl. http code)
+# arg2: httpcode (by reference)
+function get_httpreturn() {
+	local -n __http=${1}
+	local -n __res=${2}
+
+	__http="${__res:${#__res}-3}"
+    if [ ${#__res} -eq 3 ]; then
+      __res=""
+    else
+      __res="${__res:0:${#__res}-3}"
+    fi
+}
+
+# arg1: title
+# arg2: description
+function create_modelchangeset() {
+	title=${1}
+	description=${2}
+
+	# Create and open a new changeset
+	res=$(\
+		curl -sw "%{http_code}" "$sweagleURL/api/v1/model/changeset" --request POST --header "authorization: bearer $aToken"  --header 'Accept: application/vnd.siren+json' \
+		--data-urlencode "title=${title}" \
+		--data-urlencode "description=${description}")
+	# check exit code
+	rc=$?; if [ "${rc}" -ne "0" ]; then exit ${rc}; fi;
+    # check http return code
+	get_httpreturn httpcode res; if [ ${httpcode} -ne "200" ]; then exit 1; fi;
+
+    cs=$(echo ${res} | jq '.properties.changeset.id')
+	echo ${cs}
+}
+
+
+# arg1: changeset ID
+# arg2: name
+# arg3: description
+# arg4: value_type
+# arg5: required
+# arg6: sensitive
+# arg7: regex
+function create_mdi_type() {
+	changeset=${1}
+	name=${2}
+	description=${3:-}
+	value_type=${4:-Text}
+	required=${5:-false}
+	sensitive=${6:-false}
+	regex=${7:-}
+
+	# Create and open a new changeset
+	res=$(\
+		curl -sw "%{http_code}" "$sweagleURL/api/v1/model/mdiType" --request POST --header "authorization: bearer $aToken"  --header 'Accept: application/vnd.siren+json' \
+		--data "changeset=${changeset}" \
+		--data-urlencode "name=${name}" \
+		--data "required=${required}" \
+		--data-urlencode "valueType=${value_type}" \
+		--data "sensitive=${sensitive}" \
+		--data-urlencode "regex=${regex}" \
+		--data-urlencode "description=${description}")
+	# check curl exit code
+	rc=$?; if [ "${rc}" -ne "0" ]; then exit ${rc}; fi;
+    # check http return code
+	get_httpreturn httpcode res; if [ ${httpcode} -ne "200" ]; then exit 1; fi;
+
+}
+
+# arg1: changeset ID
+function approve_model_changeset() {
+	changeset=${1}
+	# Create and open a new changeset
+	res=$(curl -sw "%{http_code}" "$sweagleURL/api/v1/model/changeset/${changeset}/approve" --request POST --header "authorization: bearer $aToken"  --header 'Accept: application/vnd.siren+json')
+	# check curl exit code
+	rc=$?; if [ "${rc}" -ne "0" ]; then exit ${rc}; fi;
+    # check http return code
+	get_httpreturn httpcode res; if [ ${httpcode} -eq "200" ]; then return 0; else return 1; fi;
+}
+
+##########################################################
+#               BEGINNING OF MAIN
+##########################################################
+
+set -o errexit # exit after first line that fails
+set -o nounset # exit when script tries to use undeclared variables
+
+# load sweagle host specific variables like aToken, sweagleURL, ...
+source "sweagle.env"
+
+# create a new model changeset
+modelcs=$(create_modelchangeset 'Create MDI Type' "Create a new MDI type at $(date +'%c')")
+
+for file in ./mdi/*.props; do
+	source "$file"
+	create_mdi_type $modelcs "$name" "$description" $type $isRequired $isSensitive ${regex}
+done
+
+# approve
+approve_model_changeset ${modelcs}
+rc=$?; if [[ "${rc}" -ne 0 ]]; then echo "Model changeset approval failed"; exit ${rc}; fi
+
+exit 0
+
+# End of script
